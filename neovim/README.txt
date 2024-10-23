d407f52fde5e4d961d04035d931fa141ac24ee77
sdsaati <sdsaati@gmail.com>
2024-10-24 00:44:38 +0330

    rofi is updated

---
+++ neovim/README.txt
@@ -0,0 +1,1 @@
+to install lazyivm (portable version) you must create a link from this nvim folder to your ~/.config/
--- qtile/saati/rofi.sh
+++ qtile/saati/rofi.sh
@@ -3,4 +3,22 @@
 
 #rofi -show run -theme blue -font "ComicShannsMono Nerd Font Bold 14" -show combi
 #rofi -modi combi,drun -show drun -matching normal -theme fancy -show-icons -i -p -font "Fira Sans Bold 16"
-rofi -config ~/bin/rofi/config.rasi -show drun
+if [ -z "$1" ]; then
+  rofi -config ~/bin/rofi/config.rasi -show drun  \
+  -matching normal -show-icons -i -p \
+  -kb-accept-entry "Return,KP_Enter" \
+  -kb-row-up "Control+k" \
+  -kb-row-down "Control+j" \
+  -kb-remove-to-eol "" \
+  -kb-cancel "Escape" \
+  -kb-row-last "Control+g"
+else
+  rofi -config ~/bin/rofi/config.rasi -show "$1" \
+  -matching normal -show-icons -i -p \
+  -kb-accept-entry "Return,KP_Enter" \
+  -kb-row-up "Control+k" \
+  -kb-row-down "Control+j" \
+  -kb-remove-to-eol "" \
+  -kb-cancel "Escape" \
+  -kb-row-last "Control+g"
+fi
